15c7f94814005f3004c920964ce7d85e670f62d9
Maarten van Vliet <maartenvanvliet@gmail.com>
2022-01-28 20:19:00 +0100

    Improve tests

--- test/makeup_js_test.exs
+++ test/makeup_js_test.exs
@@ -1,63 +1,65 @@
 defmodule MakeupJsTest do
   use ExUnit.Case
 
+  import Makeup.Lexers.JsLexer.Testing, only: [lex: 1]
+
   test "whitespace" do
-    assert MakeupJs.lex("   ") == [{:whitespace, %{language: :js}, "   "}]
+    assert lex("   ") == [{:whitespace, %{}, "   "}]
   end
 
   test "single line comment" do
-    assert MakeupJs.lex("// comment") == [
-             {:comment_single, %{language: :js}, ["//", " ", "c", "o", "m", "m", "e", "n", "t"]}
+    assert lex("// comment") == [
+             {:comment_single, %{}, "// comment"}
            ]
   end
 
   test "multi line comment" do
-    assert MakeupJs.lex("""
+    assert lex("""
            /*
            comment
            /*
            """) == [
-             {:operator, %{language: :js}, "/"},
-             {:operator, %{language: :js}, "*"},
-             {:whitespace, %{language: :js}, "\n"},
-             {:name, %{language: :js}, 'comment'},
-             {:whitespace, %{language: :js}, "\n"},
-             {:operator, %{language: :js}, "/"},
-             {:operator, %{language: :js}, "*"},
-             {:whitespace, %{language: :js}, "\n"}
+             {:operator, %{}, "/"},
+             {:operator, %{}, "*"},
+             {:whitespace, %{}, "\n"},
+             {:name, %{}, "comment"},
+             {:whitespace, %{}, "\n"},
+             {:operator, %{}, "/"},
+             {:operator, %{}, "*"},
+             {:whitespace, %{}, "\n"}
            ]
   end
 
   test "integers" do
-    assert MakeupJs.lex("10") == [{:number_integer, %{language: :js}, "10"}]
-    assert MakeupJs.lex("1_000") == [{:number_integer, %{language: :js}, ["1", "_", "000"]}]
-    assert MakeupJs.lex("0888") == [{:number_integer, %{language: :js}, "0888"}]
-    assert MakeupJs.lex("0777") == [{:number_integer, %{language: :js}, "0777"}]
+    assert lex("10") == [{:number_integer, %{}, "10"}]
+    assert lex("1_000") == [{:number_integer, %{}, "1_000"}]
+    assert lex("0888") == [{:number_integer, %{}, "0888"}]
+    assert lex("0777") == [{:number_integer, %{}, "0777"}]
   end
 
   test "floats" do
-    assert MakeupJs.lex("1.440") == [{:number_float, %{language: :js}, ["1", ".", "440"]}]
+    assert lex("1.440") == [{:number_float, %{}, "1.440"}]
 
-    assert MakeupJs.lex("1_050.95") == [
-             {:number_float, %{language: :js}, ["1", "_", "050", ".", "95"]}
+    assert lex("1_050.95") == [
+             {:number_float, %{},  "1_050.95"}
            ]
   end
 
   test "keywords" do
-    assert MakeupJs.lex("new Array") == [
-             {:operator_word, %{language: :js}, [110, 101, 119]},
-             {:whitespace, %{language: :js}, " "},
-             {:name_builtin, %{language: :js}, 'Array'}
+    assert lex("new Array") == [
+             {:operator_word, %{}, "new"},
+             {:whitespace, %{}, " "},
+             {:name_builtin, %{}, "Array"}
            ]
 
-    assert MakeupJs.lex("const a = 0") == [
-             {:keyword_declaration, %{language: :js}, 'const'},
-             {:whitespace, %{language: :js}, " "},
-             {:name, %{language: :js}, 97},
-             {:whitespace, %{language: :js}, " "},
-             {:operator, %{language: :js}, "="},
-             {:whitespace, %{language: :js}, " "},
-             {:number_integer, %{language: :js}, "0"}
+    assert lex("const a = 0") == [
+             {:keyword_declaration, %{}, "const"},
+             {:whitespace, %{}, " "},
+             {:name, %{}, "a"},
+             {:whitespace, %{}, " "},
+             {:operator, %{}, "="},
+             {:whitespace, %{}, " "},
+             {:number_integer, %{}, "0"}
            ]
   end
 end
